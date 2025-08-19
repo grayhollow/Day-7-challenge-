117b87f4f4adfc35b069ad139fe80f6c8fbfdbfa
grayhollow <grayhollow7@gmail.com>
2025-08-19 19:54:38 +0300

    Finished classwork
    Assigned the variables using "let" and "const" as instructed, along with logging and creating alerts.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let name = prompt("Enter your name here:");
+let age = prompt("Enter your age here:");
+const city = prompt("Enter your city here:");
+
+alert("Hello, " + name + ". your age is " + age + ". and your city is " + city);
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
